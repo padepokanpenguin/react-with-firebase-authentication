9f03912c215e298a1318673d7627a31fc647f449
Aria Nur Jamal <jamal.psw@gmail.com>
2022-03-15 16:11:15 +0700

    redirecting user

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,8 +1,10 @@
 import {useRef, useContext} from 'react';
+import {useHistory} from 'react-router-dom';
 import AuthContext from '../../store/auth-context';
 import classes from './ProfileForm.module.css';
 
 const ProfileForm = () => {
+  const history = useHistory()
   const newPasswordInputRef = useRef();
 
   const authCtx = useContext(AuthContext)
@@ -25,6 +27,7 @@ const ProfileForm = () => {
       }
     }).then(res => {
       // assumption always succeds!
+      history.replace('/')
     })
   }
 
